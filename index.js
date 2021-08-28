25c0f06a93e8994d8bdaff340b8e9f824eafed64
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-08-28 02:49:57 +0100

    dark mode toggle
    Signed-off-by: Thomas Heaton <thomas.m.heaton@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("follow the white rabbit...");
+
+let darkMode = localStorage.getItem("darkMode") || false;
+document.body.classList.toggle(darkMode === "true" ? "dark-theme" : "");
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+  darkMode = !darkMode;
+  localStorage.setItem("darkMode", darkMode.toString())
+}
