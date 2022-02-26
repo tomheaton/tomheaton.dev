6cced043f9cf6391d0fdc39da0dba8295af9be9b
= <thomas.m.heaton@gmail.com>
2022-02-26 13:55:59 +0000

    add linkedin redirect

--- next.config.js
+++ next.config.js
@@ -4,7 +4,17 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+
+    async redirects() {
+        return [
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/tom-heaton-39b107220/',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig;
