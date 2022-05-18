23b96e4cb20cf8414437e967fce388017b9f770d
Tom Heaton <thomas.m.heaton@gmail.com>
2022-05-18 19:20:30 +0100

    more seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,38 +67,26 @@ const Index: NextPage = () => {
             <footer className={"flex justify-center text-center font-medium mt-10"}>
                 <div className={"flex flex-col"}>
                     <div className={"text-[24px] space-x-4"}>
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"https://www.github.com/tomheaton"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"https://www.github.com/tomheaton"}>
                             <i className={"bi bi-github"} aria-label={"GitHub"}/>
                         </a>
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"https://www.discord.com/users/325306360004739072"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"https://www.discord.com/users/325306360004739072"}>
                             <i className={"bi bi-discord"} aria-label={"Discord"}/>
                         </a>
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"https://www.twitter.com/tomheaton_"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"https://www.twitter.com/tomheaton_"}>
                             <i className={"bi bi-twitter"} aria-label={"Twitter"}/>
                         </a>
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"mailto:tom@tomheaton.dev"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"mailto:tom@tomheaton.dev"}>
                             <i className={"bi bi-envelope-fill"} aria-label={"Email"}/>
                         </a>
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"https://www.linkedin.com/in/tomheaton7/"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"https://www.linkedin.com/in/tomheaton7/"}>
                             <i className={"bi bi-linkedin"} aria-label={"LinkedIn"}/>
                         </a>
                     </div>
                     <p className={"p-2 text-lg"}>
                         &copy; Tom Heaton {new Date().getFullYear()}
                         {" "}
-                        <a target={"_blank"} rel={"noopener noreferrer"}
-                           href={"https://www.github.com/tomheaton/next-tomheaton-dev"}
-                        >
+                        <a target={"_blank"} rel={"me external noopener noreferrer"} href={"https://www.github.com/tomheaton/next-tomheaton-dev"}>
                             <i className={"bi bi-code-slash"} aria-label={"Source Code"}/>
                         </a>
                     </p>
