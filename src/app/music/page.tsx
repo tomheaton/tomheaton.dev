922e1c87310d7122bd9b04d9d72bf359ed5f9092
Tom Heaton <thomas.m.heaton@gmail.com>
2023-01-21 07:27:09 +0000

    fix pages

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,40 +1,31 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import type {MusicType} from "@/utils/types";
+import Footer from "@/components/Footer";
 import MusicCard from "@/components/MusicCard";
+import {musicSchema, type MusicType} from "@/utils/types";
 
 const getMusic = async () => {
     const response = await fetch("https://api.deezer.com/user/844148065/flow");
     const data = await response.json();
 
-    if (!data || !data.data) {
+    if (!data || !data.data || !data.data.length) {
         return [];
     }
 
-    let musicData: MusicType[] = [];
-
-    data.data.forEach((music: any) => {
-        if (music.type === "track") {
-            musicData.push({
-                title: music.title,
-                title_short: music.title_short,
-                duration: music.duration,
-                artist: music.artist.name,
-                link: `https://deezer.com/en/track/${music.id}`,
-                image: music.album.cover_xl,
-                album: music.album.title
-            })
+    return data.data.flatMap((m: any) => {
+        const music = musicSchema.safeParse(m);
+        if (!music.success) {
+            return [];
         }
+        return music.data;
     });
-
-    return musicData;
 }
 
 const Music = async () => {
     const data = await getMusic();
 
     return (
-        <div className={"min-h-screen bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white flex flex-col justify-between"}>
+        <div
+            className={"min-h-screen bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white flex flex-col justify-between"}>
             <main className={"h-full w-full flex flex-col content-center items-center"}>
                 <div className={"w-4/5 md:w-2/5 mt-20 min-h-96 p-10 flex flex-col border-2 border-myGreen rounded-md"}>
                     <Header/>
@@ -48,6 +39,11 @@ const Music = async () => {
                         {data && data.map((music: MusicType, index: number) => (
                             <MusicCard key={index} music={music}/>
                         ))}
+                        {data && data.length === 0 && (
+                            <p className={"col-span-2"}>
+                                no music found
+                            </p>
+                        )}
                         {!data && (
                             <p className={"col-span-2"}>
                                 loading
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,30 +1,37 @@
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import RepoCard from "@/components/RepoCard";
-import type {RepoType} from "@/utils/types";
+import {repoSchema, type RepoType} from "@/utils/types";
 
 const getProjects = async () => {
     const response = await fetch("https://gh-pinned-repos.egoist.dev/?username=tomheaton");
     const data = await response.json();
 
-    if (!data) {
+    if (!data || !data.length) {
         return [];
     }
 
-    return data;
+    return data.flatMap((r: any) => {
+        const repo = repoSchema.safeParse(r);
+        if (!repo.success) {
+            return [];
+        }
+        return repo.data;
+    });
 }
 
 const Projects = async () => {
     const data = await getProjects();
 
     return (
-        <div className={"min-h-screen bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white flex flex-col justify-between"}>
+        <div
+            className={"min-h-screen bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white flex flex-col justify-between"}>
             <main className={"h-full w-full flex flex-col content-center items-center"}>
                 <div className={"w-4/5 md:w-2/5 mt-20 min-h-96 p-10 flex flex-col border-2 border-myGreen rounded-md"}>
                     <Header/>
                     <div className={"border-t-4 my-4 border-myGreen"}/>
                     <p className={"text-xl"}>
-                        Projects (currently pinned GitHub repos)
+                        Projects
                     </p>
                 </div>
                 <div className={"w-4/5 md:w-2/5 mt-8 p-4 flex flex-col border-2 border-myGreen rounded-md"}>
@@ -32,6 +39,11 @@ const Projects = async () => {
                         {data && data.map((repo: RepoType, index: number) => (
                             <RepoCard key={index} repo={repo}/>
                         ))}
+                        {data && data.length === 0 && (
+                            <p className={"col-span-2"}>
+                                no projects found
+                            </p>
+                        )}
                         {!data && (
                             <p className={"col-span-2"}>
                                 loading
