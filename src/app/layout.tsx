b923e5bd64c8dece13ac31df17659679826a5725
Tom Heaton <thomas.m.heaton@gmail.com>
2023-04-09 13:15:16 +0100

    cleanup metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,8 @@ export const metadata: Metadata = {
     url: "https://tomheaton.dev",
   },
   keywords:
-    "software developer, software, developer, code, coding, programmer, software programmer, webapp developer, web development, programming, technology, apps, software development, software engineer, programming software, application development, software design",
+    "software developer, software, developer, code, coding, programmer, software programmer, webapp developer, web development, programming, technology, apps, software development, software engineer, programming software, application development, software design, swe, open source",
+  themeColor: "#66b193",
   openGraph: {
     title: "Tom Heaton",
     description: "Tom Heaton - Software Developer",
@@ -47,45 +48,21 @@ const RootLayout = ({ children }: PropsWithChildren) => {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag() { dataLayer.push(arguments); }
-                        gtag('js', new Date());
-                        gtag('config', '${process.env.GOOGLE_ANALYTICS_KEY}', {page_path: window.location.pathname});`,
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', '${process.env.GOOGLE_ANALYTICS_KEY}', {page_path: window.location.pathname});
+            `,
           }}
         />
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://tomheaton.dev" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
-
-        <meta charSet="utf-8" />
-
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/*<meta name="color-scheme" content="dark light" />*/}
-        {/*<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#212529" />*/}
-        {/*<meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />*/}
-        {/* <meta name="author" content="Tom Heaton" /> */}
-        {/* <meta name="description" content="Tom Heaton - Software Developer" /> */}
-        {/* <meta
-          name="keywords"
-          content="software developer, software, developer, code, coding, programmer, software programmer, webapp developer, web development, programming, technology, apps, software development, software engineer, programming software, application development, software design"
-        /> */}
-
-        {/* <meta property="og:title" content="Tom Heaton" />
-        <meta property="og:description" content="Tom Heaton - Software Developer" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://tomheaton.dev/" />
-        <meta property="og:image" content="/avatar.jpg" />
-        <meta property="og:locale" content="en_GB" /> */}
-
-        {/* <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@tomheaton_" />
-        <meta name="twitter:creator" content="@tomheaton_" />
-        <meta name="twitter:title" content="Tom Heaton" />
-        <meta name="twitter:description" content="Tom Heaton - Software Developer" />
-        <meta name="twitter:image" content="https://tomheaton.dev/avatar.jpg" />
-        <meta name="twitter:image:alt" content="Tom Heaton Avatar" /> */}
       </head>
       <body>{children}</body>
     </html>
