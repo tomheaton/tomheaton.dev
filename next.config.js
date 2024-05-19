9b4f80ee23f31aaf127904437fecce3b5dfbd428
Tom Heaton <thomas.m.heaton@gmail.com>
2024-05-19 16:28:45 +0100

    update deps + cleanup configs

--- next.config.js
+++ next.config.js
@@ -4,43 +4,42 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "opengraph.githubassets.com",
       },
       {
-        // TODO: check me
+        protocol: "https",
         hostname: "e-cdns-images.dzcdn.net",
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/linkedin",
-        destination: "https://linkedin.com/in/tomheaton7/",
-        permanent: true,
-      },
-      {
-        source: "/github",
-        destination: "https://github.com/tomheaton/",
-        permanent: true,
-      },
-      {
-        source: "/twitter",
-        destination: "https://twitter.com/tomheaton_/",
-        permanent: true,
-      },
-      {
-        source: "/x",
-        destination: "https://x.com/tomheaton_/",
-        permanent: true,
-      },
-      {
-        source: "/email",
-        destination: "mailto:tom@tomheaton.dev",
-        permanent: true,
-      },
-    ];
-  },
+  redirects: async () => [
+    {
+      source: "/linkedin",
+      destination: "https://linkedin.com/in/tomheaton7/",
+      permanent: true,
+    },
+    {
+      source: "/github",
+      destination: "https://github.com/tomheaton/",
+      permanent: true,
+    },
+    {
+      source: "/twitter",
+      destination: "https://twitter.com/tomheaton_/",
+      permanent: true,
+    },
+    {
+      source: "/x",
+      destination: "https://x.com/tomheaton_/",
+      permanent: true,
+    },
+    {
+      source: "/email",
+      destination: "mailto:tom@tomheaton.dev",
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
